884acdacfd12a65b30367c3c308a21904f8316e9
satranj2000 <sathish.ramakrishnan@gmail.com>
2021-09-19 11:43:54 +0530

    more tree related changes

--- datastructs/trees.go
+++ datastructs/trees.go
@@ -1,5 +1,7 @@
 package datastructs
 
+import "math"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -59,4 +61,50 @@ func postorderTraversal(root *TreeNode) []int {
 	nodeVal = append(nodeVal, root.Val)
 
 	return nodeVal
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	lsize := maxDepth(root.Left)
+	rsize := maxDepth(root.Right)
+
+	return int(math.Max(float64(lsize), float64(rsize))) + 1
+}
+
+//https://leetcode.com/problems/symmetric-tree/
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return isSymmetricNodes(root.Left, root.Right)
+}
+
+func isSymmetricNodes(left *TreeNode, right *TreeNode) bool {
+	if left == nil || right == nil {
+		return left == right
+	}
+
+	if left.Val != right.Val {
+		return false
+	}
+
+	return isSymmetricNodes(left.Left, right.Right) && isSymmetricNodes(left.Right, right.Left)
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+
+	lnode := invertTree(root.Left)
+	rnode := invertTree(root.Right)
+
+	root.Left = rnode
+	root.Right = lnode
+
+	return root
 }
--- datastructs/trees_test.go
+++ datastructs/trees_test.go
@@ -135,3 +135,161 @@ func TestInorderTraversal5(t *testing.T) {
 	}
 
 }
+
+func TestMaxDepth(t *testing.T) {
+
+	root := &TreeNode{10, nil, nil}
+	root.Left = &TreeNode{20, nil, nil}
+	root.Right = &TreeNode{30, nil, nil}
+	root.Left.Left = &TreeNode{40, nil, nil}
+	root.Left.Right = &TreeNode{50, nil, nil}
+	root.Right.Right = &TreeNode{60, nil, nil}
+	root.Left.Left.Left = &TreeNode{70, nil, nil}
+
+	res := maxDepth(root)
+
+	if res != 4 {
+		t.Errorf("Expected depth is 3, got %v", res)
+	}
+}
+
+func TestMaxDepth2(t *testing.T) {
+
+	root := &TreeNode{10, nil, nil}
+
+	res := maxDepth(root)
+
+	if res != 1 {
+		t.Errorf("Expected depth is 1, got %v", res)
+	}
+}
+
+func TestMaxDepth3(t *testing.T) {
+
+	res := maxDepth(nil)
+
+	if res != 0 {
+		t.Errorf("Expected depth is 1, got %v", res)
+	}
+}
+
+func TestMaxDepth4(t *testing.T) {
+
+	root := &TreeNode{10, nil, nil}
+	root.Left = &TreeNode{20, nil, nil}
+	root.Left.Left = &TreeNode{40, nil, nil}
+	root.Left.Left.Left = &TreeNode{70, nil, nil}
+
+	res := maxDepth(root)
+
+	if res != 4 {
+		t.Errorf("Expected depth is 3, got %v", res)
+	}
+}
+
+func TestMaxDepth5(t *testing.T) {
+
+	root := &TreeNode{10, nil, nil}
+	root.Right = &TreeNode{20, nil, nil}
+	root.Right.Right = &TreeNode{40, nil, nil}
+	root.Right.Right.Right = &TreeNode{70, nil, nil}
+
+	res := maxDepth(root)
+
+	if res != 4 {
+		t.Errorf("Expected depth is 3, got %v", res)
+	}
+}
+
+func TestIsSymmetric(t *testing.T) {
+	root := &TreeNode{10, nil, nil}
+	root.Right = &TreeNode{20, nil, nil}
+	root.Right.Right = &TreeNode{40, nil, nil}
+	root.Right.Right.Right = &TreeNode{70, nil, nil}
+
+	res := isSymmetric(root)
+
+	if res {
+		t.Errorf("Expected it to be false. But, got true")
+	}
+}
+
+func TestIsSymmetric2(t *testing.T) {
+	root := &TreeNode{10, nil, nil}
+	root.Right = &TreeNode{20, nil, nil}
+	root.Left = &TreeNode{20, nil, nil}
+
+	res := isSymmetric(root)
+
+	if !res {
+		t.Errorf("Expected it to be true. But, got false")
+	}
+}
+
+func TestIsSymmetric3(t *testing.T) {
+	root := &TreeNode{10, nil, nil}
+	root.Right = &TreeNode{20, nil, nil}
+	root.Left = &TreeNode{20, nil, nil}
+	root.Right.Right = &TreeNode{30, nil, nil}
+	root.Left.Left = &TreeNode{30, nil, nil}
+	root.Left.Right = &TreeNode{40, nil, nil}
+	root.Right.Left = &TreeNode{40, nil, nil}
+
+	res := isSymmetric(root)
+
+	if !res {
+		t.Errorf("Expected it to be true. But, got false")
+	}
+}
+
+func TestIsSymmetric4(t *testing.T) {
+	root := &TreeNode{10, nil, nil}
+	root.Right = &TreeNode{20, nil, nil}
+	root.Left = &TreeNode{20, nil, nil}
+	root.Right.Right = &TreeNode{30, nil, nil}
+	root.Left.Left = &TreeNode{30, nil, nil}
+	root.Left.Right = &TreeNode{41, nil, nil}
+	root.Right.Left = &TreeNode{40, nil, nil}
+
+	res := isSymmetric(root)
+
+	if res {
+		t.Errorf("Expected it to be False. But, got true")
+	}
+}
+
+func TestInvertNode(t *testing.T) {
+	root := &TreeNode{10, nil, nil}
+	root.Right = &TreeNode{2, nil, nil}
+	root.Left = &TreeNode{3, nil, nil}
+
+	resNode := invertTree(root)
+
+	expectedOutputs := []int{10, 2, 3}
+	resNodeVals := preorderTraversal(resNode)
+
+	for i, _ := range expectedOutputs {
+		if expectedOutputs[i] != resNodeVals[i] {
+			t.Errorf("Expected %v, got %v", expectedOutputs[i], resNodeVals[i])
+		}
+	}
+}
+
+func TestInvertNode2(t *testing.T) {
+	root := &TreeNode{10, nil, nil}
+	root.Left = &TreeNode{3, nil, nil}
+	root.Left.Left = &TreeNode{31, nil, nil}
+	root.Right = &TreeNode{2, nil, nil}
+	root.Right.Right = &TreeNode{21, nil, nil}
+
+	resNode := invertTree(root)
+
+	expectedOutputs := []int{10, 2, 21, 3, 31}
+	resNodeVals := preorderTraversal(resNode)
+
+	for i, _ := range expectedOutputs {
+		if expectedOutputs[i] != resNodeVals[i] {
+			t.Errorf("Expected %v, got %v", expectedOutputs[i], resNodeVals[i])
+		}
+	}
+}
